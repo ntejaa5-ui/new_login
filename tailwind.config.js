4c9f6c7f757394a06946cbb3c8f20fdcf93680b7
ntejaa5-ui <ntejaa5@gmail.com>
2026-01-03 01:24:31 +0530

    Initialize Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+```javascript
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
